5e3092bd298e59d4711e94cf32fc6837f2895dd3
reveha <sahinabdullah444@gmail.com>
2024-06-03 17:25:22 +0300

    last.

---
+++ reveha_ws/build/ball_tracker/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/reveha/reveha_ws/install/ball_tracker'
---
+++ reveha_ws/build/diffdrive_arduino/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/reveha/reveha_ws/src/diffdrive_arduino
+# Build directory: /home/reveha/reveha_ws/build/diffdrive_arduino
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
